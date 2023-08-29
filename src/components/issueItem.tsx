273a0150f5a159e1e8ea7d29163832a3edf75298
haron <haronxlee@gmail.com>
2023-08-30 00:59:22 +0900

    Feat: Detail에 Link state로 정보 보내주기, UI 수정

--- src/components/issueItem.tsx
+++ src/components/issueItem.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 import { issueProps } from 'type';
+import { CommonIssueItem, CommonTitleLayout } from 'style/Common';
 import star1 from 'assets/star1.jpeg';
 import star2 from 'assets/star2.jpeg';
 import star3 from 'assets/star3.jpeg';
@@ -13,7 +14,7 @@ interface IssueItemProps {
 }
 
 const IssueItem: FC<IssueItemProps> = ({ issue, Ad }) => {
-  const imageUrls = [star1, star2, star3, star4];
+  const imageUrls = [star3, star1, star2, star4];
 
   const currentIndex = Math.floor(issue.id % imageUrls.length);
   const imageUrl = imageUrls[currentIndex];
@@ -21,7 +22,7 @@ const IssueItem: FC<IssueItemProps> = ({ issue, Ad }) => {
   return (
     <>
       <IssueItemLayout>
-        <IssueItemLink to={'/' + issue.id}>
+        <IssueItemLink to={'/' + issue.id} state={issue}>
           <IssueTitleLayout>
             <h2>
               <strong>{issue.number}</strong> {issue.title}
@@ -35,7 +36,9 @@ const IssueItem: FC<IssueItemProps> = ({ issue, Ad }) => {
       </IssueItemLayout>
       {Ad && (
         <AdImage>
-          <img src={imageUrl} alt='광고 이미지' />
+          <Link to='https://www.wanted.co.kr/'>
+            <img src={imageUrl} alt='광고 이미지' />
+          </Link>
         </AdImage>
       )}
     </>
@@ -43,9 +46,8 @@ const IssueItem: FC<IssueItemProps> = ({ issue, Ad }) => {
 };
 
 const IssueItemLayout = styled.li`
-  border-bottom: 1px solid var(--gray-800);
-  padding: 40px 0;
-  padding-right: 20px;
+  ${CommonIssueItem}
+  list-style: none;
 
   &:hover {
     background-color: var(--secondary);
@@ -62,31 +64,18 @@ const IssueItemLink = styled(Link)`
 `;
 
 const IssueTitleLayout = styled.div`
-  h2 {
-    margin-bottom: 20px;
-    font-size: 20px;
-    font-weight: bold;
-    overflow: hidden;
-    white-space: normal;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 1;
-    -webkit-box-orient: vertical;
-    word-break: keep-all;
-
-    strong {
-      color: var(--primary);
-    }
-  }
-
-  p {
-    color: var(--gray-500);
-  }
+  ${CommonTitleLayout}
 `;
 
 const AdImage = styled.li`
   width: 100%;
   height: 160px;
+  list-style: none;
+
+  a {
+    width: 100%;
+    height: 100%;
+  }
 
   img {
     width: 100%;
